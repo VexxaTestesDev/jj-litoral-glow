242d6f81b89488d0ca56b5e523ee905dbeb6850f
Vitor <vitorthomaz111@gmail.com>
2026-06-25 21:53:47 -0300

    git ignore removido

---
+++ .output/server/_tanstack-start-manifest_v-BQ_pBgLP.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BQ_pBgLP.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/vitor/OneDrive/Documents/GIT/jj-litoral-glow/src/routes/__root.tsx",
+		children: ["/", "/sitemap.xml"],
+		preloads: ["/assets/index-DcV_biCa.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DcV_biCa.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/vitor/OneDrive/Documents/GIT/jj-litoral-glow/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BQS34TxU.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
